9b9dbc66822583c69b934cfe8e8d181339a8a517
Dian Citra <diancitra1409@gmail.com>
2026-02-17 14:14:57 +0700

    pesan kamu nambah atau rubah apa

---
+++ src/page/Service.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Service.css";
+
+function Service() {
+
+  const cards = [
+    {
+      title: "Rain Shaman",
+      desc: "Weather Control with an Intelligent Prediction System."
+    },
+    {
+      title: "Real-Time Dat",
+      desc: "Monitor Today’s Weather."
+    },
+    {
+      title: "Forecast",
+      desc: "Upcoming Weather Forecast."
+    },
+    {
+      title: "History",
+      desc: "Previous Day Weather History."
+    }
+
+    
+  ];
+
+  return (
+    <div className="service-container">
+
+      <nav className="navbar">
+  <h2></h2>
+
+  <div className="nav-links">
+    <span>Home </span>
+    <span>Address </span>
+    <span>contact </span>
+  </div>
+</nav>
+
+  <div className="rain">
+  {Array.from({ length: 120 }).map((_, i) => (
+    <div
+      key={i}
+      className="drop"
+      style={{
+        left: Math.random() * 100 + "%",
+        animationDuration: 0.7 + Math.random() * 1.5 + "s",
+        opacity: Math.random()
+      }}
+    />
+  ))}
+</div>
+
+
+
+      {/* HEADER */}
+      <header className="service-header">
+        <h1>Natanegara Langit</h1>
+        <p>We Predicted The Sky Before It Moves</p>
+      </header>
+
+            {/* CARDS */}
+      <div className="card-wrapper">
+  {cards.map((card, index) => (
+    <div className="card" key={index}>
+      <h3>{card.title}</h3>
+      <p>{card.desc}</p>
+      <button>View Details →</button>
+    </div>
+  ))}
+</div>
+
+<footer className="footer">
+  © 2026 Natanegara Langit
+</footer>
+          
+    </div>
+  );
+  
+}
+
+
+
+export default Service;
