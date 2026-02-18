84f5c5d73db3981ed5ecb9952e3989ca8481dc20
Ro'uf Aufalin Al Ghoitsal <roufaufal01@gmail.com>
2026-02-18 11:19:19 +0700

    mau pull dulu (sowreyyy commitnya jelek)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
-import Home from "./pages/Home";
-import Service from "./pages/Service";
-import Weather from "./pages/Weather";
-import Contact from "./pages/Contact";
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./component/Navbar"
+import Home from "./pages/Home"
+// import Service from "./pages/Service"
+// import Weather from "./pages/Weather"
+import BookingPage from "./pages/BookingPage"
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/service" element={<Service />} />
+        <Route path="/" element={<BookingPage />} />
+        {/* <Route path="/service" element={<Service />} />
         <Route path="/weather" element={<Weather />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<BookingPage />} /> */}
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
